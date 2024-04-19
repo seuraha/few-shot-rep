808a1a521611694209c1d0059d7dc54ef9b4478c
Seura Ha <seura@m-fvfg63b6q6lw.local>
2024-04-19 15:59:15 -0400

    initial commit - parameters and structure

---
+++ params_low-dim-linear-rep.py
@@ -0,0 +1,7 @@
+d = 100 # input space dimension
+k = 20 # feature space dimension
+yd = 1 # output space dimension
+nT = 20 # number of source tasks
+n1 = 1000 # sample size for each source task
+n2 = 1000 # sample size for target task
+
